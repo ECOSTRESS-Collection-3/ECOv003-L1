a027cfc239d71ca134f718b18aebd70628315165
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2015-11-12 10:55:27 -0800

    Add in L1aPixGenerate.

---
+++ python/lib/l1a_pix_generate.py
@@ -0,0 +1,19 @@
+from geocal import *
+import h5py
+import shutil
+
+class L1aPixGenerate(object):
+    '''This generates a L1A pix file from the given L1A_BB and L1A_RAW
+    files.'''
+    def __init__(self, l1a_bb, l1a_raw, output_name):
+        '''Create a L1aPixgenerate with the given inut files
+        and output file name. To actually generate, execute the 'run'
+        command.'''
+        self.l1a_bb = l1a_bb
+        self.l1a_raw = l1a_raw
+        self.output_name = output_name
+
+    def run(self):
+        '''Do the actual generation of data.'''
+        shutil.copyfile(self.l1a_raw, self.output_name)
+
---
+++ python/lib/l1a_pix_generate_test.py
@@ -0,0 +1,17 @@
+from nose.tools import *
+from nose.plugins.skip import Skip, SkipTest
+from l1a_pix_generate import *
+
+# Right now depend on end to end testing. May want to have a subset
+# of this defined at some point
+test_data = "/project/ancillary/ASTER/EndToEndTest/"
+
+l1a_bb = test_data + "ECOSTRESS_L1A_BB_800001_00001_20151024020211_0100_01.h5"
+l1a_raw = test_data + "ECOSTRESS_L1A_RAW_800001_00001_20151024020211_0100_01.h5"
+
+def test_l1a_pix_generate():
+    l1bpix = L1aPixGenerate(l1a_bb, l1a_raw, "l1a_pix.h5")
+    l1bpix.run()
+
+
+
