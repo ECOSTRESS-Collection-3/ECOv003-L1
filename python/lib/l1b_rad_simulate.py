29ee320cb358af737c38a28673c2a0d8503fb0ae
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2016-05-31 16:01:41 -0700

    Add L1bRadSimulate to generating test data.

---
+++ python/lib/l1b_rad_simulate.py
@@ -0,0 +1,88 @@
+from geocal import *
+import pickle
+import ecostress.pickle_method
+from multiprocessing import Pool
+import h5py
+from ecostress.write_standard_metadata import WriteStandardMetadata
+
+class L1bRadSimulate(object):
+    '''This is used to generate simulated input data for the L1bGeoGenerate 
+    process, for use in testing.'''
+    def __init__(self, orbit, time_table, camera, surface_image, dem = None,
+                 number_integration_step = 1, raycast_resolution = -1):
+        '''Create a L1bGeoSimulate that works with the given orbit, time table,
+        camera, and surface images. surface_image should have one image for each band.
+
+        The default DEM to use is the SRTM, but you can pass in a different DEM if 
+        desired.
+
+        The default resolution is whatever the underlying surface image is at, but 
+        you can specify a different resolution. Note that this has a strong impact
+        on how long the data takes to generate. The ASTER data is 15 meter resolution,
+        which means that the 70 meter ecostress pixels takes 5 x 5 subpixels. For
+        a much quicker simulation, you can set this to something like 100 meters.'''
+        self.orbit = orbit
+        self.time_table = time_table
+        self.camera = camera
+        self.surface_image = surface_image
+        self.dem = dem
+        self.number_integration_step = number_integration_step
+        self.raycast_resolution = raycast_resolution
+        if(self.dem is None):
+            # False here says we use a height of 0 for missing tiles, useful
+            # for data that is partially over the ocean
+            self.dem = SrtmDem("", False)
+
+    def create_file(self, l1b_rad_fname, pool = None):
+        fout = h5py.File(l1b_rad_fname, "w")
+        g = fout.create_group("Radiance")
+        for b in range(6):
+        # We hold camera_band to 0 for now. We'll improve this and use actually
+        # other bands in the future as we flesh out the simulation.
+            t = g.create_dataset("radiance_%d" % (b + 1),
+                                 data = self.image(b, camera_band = 0, pool = pool))
+            if(b != 5):
+                t.attrs["Units"] = "W/m^2/sr/um"
+            else:
+                t.attrs["Units"] = "dimensionless"
+        m = WriteStandardMetadata(fout, product_specfic_group = "L1B_RADMetadata",
+                                  pge_name = "L1B_CAL")
+        m.write()
+                
+    def image_parallel_func(self, it):
+        '''Calculate image for a subset of the data, suitable for use with a
+        multiprocessor pool.'''
+        # Handle number_sample too large here, so we don't have to
+        # have special handling elsewhere
+        start_sample = it[0]
+        nleft = self.igc_ray_cast.number_sample - start_sample
+        number_sample = min(it[1], nleft)
+        return self.igc_ray_cast.read_double(0, start_sample, self.igc_ray_cast.number_line, number_sample)
+    
+    def image(self, band, camera_band = None, pool = None):
+        '''Use raycasting to generate a image for the given band. By default the
+        camera_band is the same as the band, but you can optionally set it to a
+        different value.
+
+        Note that this takes about 5 minutes over ASTER data, using 20 processors
+        (at 15 meter resolution)'''
+        print("Doing band %d" % (band + 1))
+        if(camera_band is None):
+            camera_band = band
+        ipi = Ipi(self.orbit, self.camera, camera_band, self.time_table.min_time,
+                  self.time_table.max_time, self.time_table)
+        igc = IpiImageGroundConnection(ipi, self.dem, None)
+        self.igc_ray_cast = IgcSimulatedRayCaster(igc, self.surface_image[band],
+                                                  self.number_integration_step,
+                                                  self.raycast_resolution)
+        if(pool is None):
+            return self.image_parallel_func([0,self.igc_ray_cast.number_sample])
+        nprocess = pool._processes
+        n = math.floor(self.igc_ray_cast.number_sample / nprocess)
+        if(self.igc_ray_cast.number_sample % nprocess > 0):
+            n += 1
+        it = [[i,n] for i in range(0,self.igc_ray_cast.number_sample, n)]
+        r = pool.map(self.image_parallel_func, it)
+        return np.hstack(r)
+
+        
---
+++ python/lib/l1b_rad_simulate_test.py
@@ -0,0 +1,39 @@
+from nose.tools import *
+from nose.plugins.skip import Skip, SkipTest
+from geocal import *
+from l1b_rad_simulate import *
+from multiprocessing import Pool
+
+# Right now depend on end to end testing. May want to have a subset
+# of this defined at some point
+test_data = os.path.dirname(__file__) + "/../../end_to_end_testing/"
+
+orb = read_shelve(test_data + "orbit.xml")
+cam = read_shelve(test_data + "camera.xml")
+tt = read_shelve(test_data + "time_table.xml")
+band = 0
+def fname(band):
+        return "/data/smyth/AsterMosiac/calnorm_b%d.img" % band
+sdata = [VicarLiteRasterImage(fname(aster_band)) for aster_band in [14, 14, 12, 11, 10, 4]]
+
+def test_l1b_rad_simulate():
+    raise SkipTest  # Don't normally run this, it takes a while
+    # Only do 100 lines so this runs quickly as a test
+    l1b_sim = L1bRadSimulate(orb, tt, cam, sdata, raycast_resolution = 100.0)
+    # Limitation of test data is that it expects to be in end_to_end_testing
+    # directory because of relative paths.
+    curdir = os.getcwd()
+    # Bug that we seem to need to do this to force spice to be loaded before
+    # we create the pool. Would like to figure out why this happens and fix this
+    SpiceHelper.spice_setup()
+    try:
+        os.chdir(test_data)
+        pool = Pool(20)
+        l1b_sim.create_file("ECOSTRESS_L1B_RAD_800001_00001_20151024020211_0100_01.h5",
+                            pool=pool)
+    finally:
+        os.chdir(curdir)
+    
+
+
+
