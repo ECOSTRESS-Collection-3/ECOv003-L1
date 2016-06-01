586904f72acaaf19caf00dd161e6b0644a2bbbe9
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2016-05-31 17:12:08 -0700

    Checkpoint updates to generation of end to end test data.

--- end_to_end_testing/create_end_to_end_test_data.py
+++ end_to_end_testing/create_end_to_end_test_data.py
@@ -1,9 +1,8 @@
-# This creates test data end to end. This is the content of the
-# original notebook we used, put into a script that is easier to 
-# run in the future.
-
+# This creates test data end to end.
 from geocal import *
+from ecostress import *
 import h5py
+from multiprocessing import Pool
 
 # Center times for each of the passes. See the wiki at 
 # https://wiki.jpl.nasa.gov/display/ecostress/Test+Data and subpages for details
@@ -13,17 +12,24 @@ pass_time = [Time.parse_time("2015-01-14T12:00:24.995464Z"),
             Time.parse_time("2015-03-02T16:34:33.146760Z"),
             Time.parse_time("2015-04-16T22:13:14.347679Z"),
             Time.parse_time("2015-01-24T14:43:18.819553Z")]
-pass_index = 0 # The pass we are working with
+orbit_num = [80001, 80002, 80003, 80004, 80005]
+nscene = [1,1,1,1,3]
+
+pass_index = 4 # The pass we are working with
 
 # Get the ASTER mosaic we are working with for all the bands. The
 # bands comes from the wiki. Data originally comes from /raid11/astermos,
 # but is compressed there.
-generate_raycasted_data = True
-if(generate_raycasted_data):
-    # Only on pistol
-    def fname(band):
-        return "/data/smyth/AsterMosiac/calnorm_b%d.img" % band
-    f = [VicarLiteRasterImage(fname(aster_band)) for aster_band in [10, 11, 12, 14, 14, 4]]
+# (data is only on pistol
+def fname(band):
+    return "/data/smyth/AsterMosiac/calnorm_b%d.img" % band
+# These scale factors comes from https://lpdaac.usgs.gov/dataset_discovery/aster/aster_products_table/ast_l1t
+scale_factor = [1.688, 1.415, 0.862, 0.2174, 0.0696, 0.0625, 0.0597, 0.0417, 0.0318,
+                6.882e-3, 6.780e-3, 6.590e-3, 5.693e-3, 5.225e-3]
+sdata = [ScaleImage(VicarLiteRasterImage(fname(aster_band), 1, VicarLiteFile.READ,
+                                         5000, 5000),
+                    scale_factor[aster_band-1])
+         for aster_band in [14, 14, 12, 11, 10, 4]]
 
 orb = SpiceOrbit(SpiceOrbit.ISS_ID, "iss_spice/iss_2015.bsp")
 
@@ -46,41 +52,27 @@ cam = QuaternionCamera(frame_to_sc, 1, 5400, 40e-3 * 1.8, 40e-3 * 2,
 # that combines 2 pixels. So we have the factor of 2 given. Scene has 
 # 5400 pixels, which is where time calcuation comes from.
 
-# False here says it ok for SrtmDem to not have tile. This gives support
-# for data that is over the ocean.
-dem = SrtmDem("",False)
-# Don't treat the bands as different yet, so fix this to a single band
-band = 0
 tspace = 1.181 / 241 * 2
 toff = 5400 * tspace / 2
-tt = ConstantSpacingTimeTable(pass_time[pass_index] - toff + tspace, 
-                              pass_time[pass_index] + toff, tspace)
-ipi = Ipi(orb, cam, band, tt.min_time, tt.max_time, tt)
-igc = IpiImageGroundConnection(ipi, dem, None)
-
-print(distance(igc.ground_coordinate(ImageCoordinate(1, 5400 / 2)),
-               igc.ground_coordinate(ImageCoordinate(1, 1 + 5400 / 2))))
-print(distance(igc.ground_coordinate(ImageCoordinate(1, 5400 / 2)),
-               igc.ground_coordinate(ImageCoordinate(2, 5400 / 2))))
-print(igc.footprint_resolution(1, int(5400 / 2)))
-
-# We may want to have IgcSimulated not read everything into memory. But 
-# start with this
-
-if(generate_raycasted_data):
-    for b in range(6):
-        write_shelve("test_data.db:igc_simulated_p%d_b%d" % (pass_index + 1, b+1), 
-                     IgcSimulated(igc, f[b], -1, False))
-
-# Then do 
-# write_image --verbose --number-process=20 --process-number-line=100 --process-number-sample=100 test_data.db:igc_simulated_p1_b1 l1b1_sim_p1_b1.img
-# And repeat for all bands
-
+tlen = 5400 * tspace
+# Bug that we seem to need to do this to force spice to be loaded before
+# we create the pool. Would like to figure out why this happens and fix this
+SpiceHelper.spice_setup()
+pool = Pool(20)
+for s in range(nscene[pass_index]):
+    tt = ConstantSpacingTimeTable(pass_time[pass_index] - toff + tspace +
+                                  s * tlen, 
+                                  pass_time[pass_index] + toff + s * tlen,
+                                  tspace)
+    fname = ecostress_file_name("L1B_RAD", orbit_num[pass_index], s + 1, tt.min_time)
+    t = L1bRadSimulate(orb, tt, cam, sdata, raycast_resolution = 100.0)
+    t.create_file(fname, pool = pool)
 
 # Probably don't want these long term, but save for now
-write_shelve("orbit.xml", orb)
-write_shelve("camera.xml", cam)
-write_shelve("time_table.xml", tt)
+if(False):
+    write_shelve("orbit.xml", orb)
+    write_shelve("camera.xml", cam)
+    write_shelve("time_table.xml", tt)
 
 # fout = h5py.File("ECOSTRESS_L1A_RAW_ATT_800001_00001_20151024020211_0100_01.h5", "w")
 # g = fout.create_group("DummyData")
