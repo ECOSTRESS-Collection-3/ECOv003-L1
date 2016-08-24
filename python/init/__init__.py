f2cc0d740b678e8f62deb13ae3b864935b3ab1fc
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2016-08-23 17:22:09 -0700

    Added a test use of cython code in the library, and found that we need to reverse the order we load things in __init__.py. Believe this is working now.

--- python/init/__init__.py
+++ python/init/__init__.py
@@ -4,15 +4,16 @@
 import os
 import glob
 
+# Load cython stuff
+import ecostress._ecostress_level1
+for mname in ecostress._cython_module_list:
+    exec("from %s import *" % mname)
+
 for i in glob.glob(os.path.dirname(__file__) + "/*.py"):
     mname = os.path.basename(i).split('.')[0]
     # Don't load ipython, which is ipython magic extensions
     if(not mname == 'ipython'):
         exec("from .%s import *" % mname)
 
-# Load cython stuff
-import ecostress._ecostress_level1
-for mname in ecostress._cython_module_list:
-    exec("from %s import *" % mname)
     
 
---
+++ python/lib/cython_try.py
@@ -0,0 +1,5 @@
+from .cython_sample import fib
+
+def cython_try():
+    print("hi there")
+    print(fib(10))
