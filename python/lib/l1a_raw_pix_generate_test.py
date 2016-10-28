d179601670ed605fbdcf53002ee3c6227378065d
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2016-10-28 16:24:58 -0700

    Fix a couple of unit tests

--- python/lib/l1a_raw_pix_generate_test.py
+++ python/lib/l1a_raw_pix_generate_test.py
@@ -4,7 +4,7 @@ import os
 
 
 def test_l1a_raw_pix_generate(isolated_dir, test_data):
-    l0 = test_data + "ECOSTRESS_L0_20150124T144252_0100_01.raw"
+    l0 = test_data + "ECOSTRESS_L0_20150124T204251_0100_01.raw"
     l1arawpix = L1aRawPixGenerate(l0)
     l1arawpix.run()
 
--- python/lib/l1b_rad_generate_test.py
+++ python/lib/l1b_rad_generate_test.py
@@ -2,9 +2,10 @@ from .l1b_rad_generate import *
 from test_support import *
 
 def test_l1b_rad_generate(isolated_dir, test_data):
-    l1a_pix = test_data + "ECOSTRESS_L1A_PIX_80005_001_20150124T144252_0100_01.h5.expected"
-    l1brad = L1bRadGenerate(l1a_pix, "l1b_rad.h5",
-                            local_granule_id = "ECOSTRESS_L1B_RAD_80005_001_20150124T144252_0100_01.h5")
+    l1a_pix = test_data + "ECOSTRESS_L1A_PIX_80005_001_20150124T204251_0100_01.h5.expected"
+    l1a_gain = test_data + "ECOSTRESS_L1A_TEMPORARY_GAIN_80005_001_20150124T204251_0100_01.h5.expected"
+    l1brad = L1bRadGenerate(l1a_pix, l1_gain,
+                            "ECOSTRESS_L1B_RAD_80005_001_20150124T204251_0100_01.h5"
     l1brad.run()
 
 
