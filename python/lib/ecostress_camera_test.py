46bf4b2c600a1493ec9a672df0be24837b05af00
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2016-12-01 13:52:03 -0800

    Have a basic test in place of the swig code.

---
+++ python/lib/ecostress_camera_test.py
@@ -0,0 +1,27 @@
+try:
+    from ecostress_swig import *
+except ImportError:
+    raise RuntimeError("You need to install the ecostress swig code first. You can install just this by doing 'make install-swig-python'")
+from geocal import *
+from test_support import *
+
+def test_basic():
+    '''Make sure we can create a camera, and print it out.'''
+    cam = EcostressCamera()
+    print(cam)
+
+def test_camera_use_test(igc):
+    '''Basic test of have camera used by geocal.'''
+    ic = ImageCoordinate(0,0)
+    t, fc = igc.ipi.time_table.time(ic)
+    od = igc.ipi.orbit.orbit_data(t)
+    dem = igc.dem
+    cam = EcostressCamera()
+    gp1 = od.surface_intersect(cam, fc, dem)
+    fc.line = fc.line + 1
+    gp2 = od.surface_intersect(cam, fc, dem)
+    fc.line = fc.line - 1
+    fc.sample = fc.sample + 1
+    gp3 = od.surface_intersect(cam, fc, dem)
+    print(distance(gp1, gp2))
+    print(distance(gp1, gp3))    
