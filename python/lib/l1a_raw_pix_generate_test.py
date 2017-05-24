5809b824cc789f661a0153cca23f2f8342d05827
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2017-05-24 09:58:33 -0700

    Have end to end check running with new l1a_raw.

--- python/lib/l1a_raw_pix_generate_test.py
+++ python/lib/l1a_raw_pix_generate_test.py
@@ -4,10 +4,10 @@ import os
 
 @slow
 def test_l1a_raw_pix_generate(isolated_dir, test_data):
-    l0 = test_data + "ECOSTRESS_L0B_20150124T204251_20150124T204533_0100_01.h5"
-    osp_dir = test_data + "l1_osp_dir"
+    l0b = test_data + "ECOSTRESS_L0B_20150124T204251_20150124T204533_0100_01.h5"
+    l1_osp_dir = test_data + "l1_osp_dir"
     scene_file = test_data + "Scene_80005_20150124T204251_20150124T204533.txt"
-    l1arawpix = L1aRawPixGenerate(l0, osp_dir, scene_file)
+    l1arawpix = L1aRawPixGenerate(l0b, l1_osp_dir, scene_file)
     l1arawpix.run()
 
 
