a4966132be76443ba012417fd00115f09389bb69
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2018-03-09 09:10:13 -0800

    Add unit test to check various jacobian calculations in EcostressTimeTable. Everything works fine, just have tests now to demonstrate this.

--- lib/ecostress_time_table_test.cc
+++ lib/ecostress_time_table_test.cc
@@ -57,6 +57,89 @@ BOOST_AUTO_TEST_CASE(basic_test)
   BOOST_CHECK_EQUAL(lend, 128 * 4);
 }
 
+BOOST_AUTO_TEST_CASE(time_jac_test)
+{
+  GeoCal::Time tstart = GeoCal::Time::parse_time("2015-01-24T20:42:52Z");
+  EcostressTimeTable tt(tstart);
+  GeoCal::ImageCoordinateWithDerivative ic;
+  ic.line = GeoCal::AutoDerivative<double>(5, 0, 2);
+  ic.sample = GeoCal::AutoDerivative<double>(10, 1, 2);
+  GeoCal::TimeWithDerivative t;
+  GeoCal::FrameCoordinateWithDerivative fc;
+  tt.time_with_derivative(ic, t, fc);
+  blitz::Array<double, 2> jac_calc(3,2);
+  jac_calc(0, blitz::Range::all()) = t.gradient();
+  if(fc.line.is_constant())
+    jac_calc(1, blitz::Range::all()) = 0;
+  else
+    jac_calc(1, blitz::Range::all()) = fc.line.gradient();
+  if(fc.sample.is_constant())
+    jac_calc(2, blitz::Range::all()) = 0;
+  else
+    jac_calc(2, blitz::Range::all()) = fc.sample.gradient();
+  GeoCal::Time t0;
+  GeoCal::FrameCoordinate fc0;
+  tt.time(GeoCal::ImageCoordinate(5,10), t0, fc0);
+  blitz::Array<double, 2> jac_fd(3,2);
+  GeoCal::Time t1;
+  GeoCal::FrameCoordinate fc1;
+  double step = 0.1;
+  tt.time(GeoCal::ImageCoordinate(5+step,10), t1, fc1);
+  jac_fd(0,0) = (t1-t0)/step;
+  jac_fd(1,0) = (fc1.line - fc0.line) / step;
+  jac_fd(2,0) = (fc1.sample - fc0.sample) / step;
+  tt.time(GeoCal::ImageCoordinate(5,10+step), t1, fc1);
+  jac_fd(0,1) = (t1-t0)/step;
+  jac_fd(1,1) = (fc1.line - fc0.line) / step;
+  jac_fd(2,1) = (fc1.sample - fc0.sample) / step;
+  // std::cerr << jac_calc << "\n"
+  // 	    << jac_fd << "\n";
+  BOOST_CHECK_MATRIX_CLOSE_TOL(jac_calc, jac_fd, 1e-4);
+}
+
+BOOST_AUTO_TEST_CASE(image_coordinate_jac_test)
+{
+  GeoCal::Time tstart = GeoCal::Time::parse_time("2015-01-24T20:42:52Z");
+  EcostressTimeTable tt(tstart);
+  GeoCal::Time t0;
+  GeoCal::FrameCoordinate fc0;
+  tt.time(GeoCal::ImageCoordinate(5,10), t0, fc0);
+  GeoCal::TimeWithDerivative t = GeoCal::TimeWithDerivative::time_pgs
+    (GeoCal::AutoDerivative<double>(t0.pgs(), 0, 3));
+  GeoCal::FrameCoordinateWithDerivative fc;
+  fc.line = GeoCal::AutoDerivative<double>(fc0.line, 1, 3);
+  fc.sample = GeoCal::AutoDerivative<double>(fc0.sample, 2, 3);
+  GeoCal::ImageCoordinateWithDerivative ic =
+    tt.image_coordinate_with_derivative(t, fc);
+  blitz::Array<double, 2> jac_calc(2,3);
+  if(ic.line.is_constant())
+    jac_calc(0, blitz::Range::all()) = 0;
+  else
+    jac_calc(0, blitz::Range::all()) = ic.line.gradient();
+  if(ic.sample.is_constant())
+    jac_calc(1, blitz::Range::all()) = 0;
+  else
+    jac_calc(1, blitz::Range::all()) = ic.sample.gradient();
+  blitz::Array<double, 2> jac_fd(2,3);
+  GeoCal::ImageCoordinate ic0 = tt.image_coordinate(t0, fc0);
+  double step = 0.1;
+  GeoCal::Time t1 = t0 + step;
+  GeoCal::ImageCoordinate ic1 = tt.image_coordinate(t1, fc0);
+  jac_fd(0,0) = (ic1.line - ic0.line) / step;
+  jac_fd(1,0) = (ic1.sample - ic0.sample) / step;
+  GeoCal::FrameCoordinate fc1(fc0.line + step, fc0.sample);
+  ic1 = tt.image_coordinate(t0, fc1);
+  jac_fd(0,1) = (ic1.line - ic0.line) / step;
+  jac_fd(1,1) = (ic1.sample - ic0.sample) / step;
+  GeoCal::FrameCoordinate fc2(fc0.line, fc0.sample + step);
+  ic1 = tt.image_coordinate(t0, fc2);
+  jac_fd(0,2) = (ic1.line - ic0.line) / step;
+  jac_fd(1,2) = (ic1.sample - ic0.sample) / step;
+  // std::cerr << jac_calc << "\n"
+  // 	    << jac_fd << "\n";
+  BOOST_CHECK_MATRIX_CLOSE_TOL(jac_calc, jac_fd, 1e-2);
+}
+
 BOOST_AUTO_TEST_CASE(read_file)
 {
   EcostressTimeTable tt(test_data_dir() + "ECOSTRESS_L1A_PIX_80005_001_20150124T204250_0100_02.h5.expected");
