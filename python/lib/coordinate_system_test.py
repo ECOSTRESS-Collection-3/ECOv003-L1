5a9606dd7a5c85d06e0d9dc4627d1fedbf1f253b
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2018-05-16 09:49:42 -0700

    Add in coordinate_system.py. We don't actually do anything with this, but it figures out the over all offset to apply in our position correction based on the ATB

---
+++ python/lib/coordinate_system_test.py
@@ -0,0 +1,7 @@
+from .coordinate_system import *
+from test_support import *
+import numpy as np
+
+def test_coordinate_system():
+    print(m_jem_to_a * m_ef_to_jem * m_10_to_ef * m_optics_to_10 * m_camera_to_optics)
+    
