24463e089096bbfe3c45ccd2011efa58c5e925e6
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2018-05-24 17:11:30 -0700

    Add generation of attitude/ephemeris updated data. This closes #49.

--- python/lib/l1b_att_generate.py
+++ python/lib/l1b_att_generate.py
@@ -7,12 +7,14 @@ import numpy as np
 
 class L1bAttGenerate(object):
     '''This generates the L1B att output file from the given 
-    ImageGroundConnection. This should have already had the orbit updated.
+    corrected orbit. 
 
     Note that despite the name, this is actually both attitude and ephemeris.
     '''
-    def __init__(self, igc, output_name, run_config = None,
-                 local_granule_id = None):
+    def __init__(self, orbcorr, output_name, tatt, teph, inlist,
+                 run_config = None, local_granule_id = None,
+                 build_id = "0.30",
+                 pge_version = "0.30"):
         '''Create a L1bAttGenerate with the given ImageGroundConnection
         and output file name. To actually generate, execute the 'run'
         command.
@@ -21,59 +23,64 @@ class L1bAttGenerate(object):
         metadata. Without this, we skip that metadata and just have fill data.
         This is useful for testing, but for production you'll always want to 
         have the run config available.'''
-        self.igc = igc
+        self.orbcorr = orbcorr
         self.output_name = output_name
+        self.tatt = tatt
+        self.teph = teph
         self.run_config = run_config
         self.local_granule_id = local_granule_id
-
-    def sample_data(self):
-        '''Return the sampled data.'''
-        # We may want to get this from somewhere, but for now just hardcode
-        # this
-        tt = self.igc.ipi.time_table
-        orb = self.igc.ipi.orbit
-        rate = 1.181
-        tstart = tt.min_time
-        tend = tt.max_time
-        npt = int(math.ceil((tend-tstart) / rate))
-        tm = [tstart + i * rate for i in range(npt + 1)]
-        tj2000 = [t.j2000 for t in tm]
-        pos = np.array([orb.position_ci(t).position for t in tm])
-        vel = np.array([orb.orbit_data(t).velocity_ci for t in tm])
-        att = np.array([geocal.quaternion_to_array(orb.orbit_data(t).sc_to_ci) 
-                        for t in tm])
-        return tj2000, pos, vel, att
+        self.build_id = build_id
+        self.pge_version = pge_version
+        self.inlist = inlist
 
     def run(self):
         '''Do the actual generation of data.'''
         fout = h5py.File(self.output_name, "w")
-        m = WriteStandardMetadata(fout, "L1GEOMetadata",
-                                  pge_name="L1B_GEO_PGE",
-                                  build_id = '0.30', pge_version='0.30',
+        m = WriteStandardMetadata(fout,
+                                  product_specfic_group = "L1GEOMetadata",
+                                  proc_lev_desc = "Level 1B Geolocation Parameters",                                  
+                                  pge_name="L1B_GEO",
+                                  build_id = self.build_id,
+                                  orbit_based = True,
+                                  pge_version= self.pge_version,
                                   local_granule_id = self.local_granule_id)
-        
-        dt, tm = time_split(self.igc.ipi.min_time)
-        m.set("RangeBeginningDate", dt)
-        m.set("RangeBeginningTime", tm)
-        dt, tm = time_split(self.igc.ipi.max_time)
-        m.set("RangeEndingDate", dt)
-        m.set("RangeEndingTime", tm)
         if(self.run_config is not None):
             m.process_run_config_metadata(self.run_config)
-        g = fout.create_group("L1bAtt")
-        tj2000, posd, veld, attd = self.sample_data()
-        tm = g.create_dataset("time", data=tj2000, dtype="f8")
-        pos = g.create_dataset("position", data=posd, dtype="f8")
-        vel = g.create_dataset("velocity", data=veld, dtype="f8")
-        att = g.create_dataset("attitude_quaternion", data=attd, dtype="f8")
-        tm.attrs["Description"] = "J2000 time"
-        tm.attrs["Units"] = "second"
-        pos.attrs["Description"] = "ECI position"
-        pos.attrs["Units"] = "meter"
-        vel.attrs["Description"] = "ECI velocity"
-        vel.attrs["Units"] = "meter/second"
-        att.attrs["Description"] = "Attitude quaternion. This gives orientation of the spacecraft with the ECI coordinate system."
-        att.attrs["Units"] = "dimensionless"
+        m.set("ImageLines", 0)
+        m.set("ImagePixels", 0)
+        dt, tm = time_split(self.orbcorr.min_time)
+        m.set("RangeBeginningDate", dt)
+        m.set("RangeBeginningTime", tm)
+        dt, tm = time_split(self.orbcorr.max_time)
+        m.set("RangeEndingDate", dt)
+        m.set("RangeEndingTime", tm)
+        m.set_input_pointer(self.inlist)
+        g = fout.create_group("Attitude")
+        t = g.create_dataset("time_j2000",
+                             data=np.array([t.j2000 for t in self.tatt]))
+        t.attrs["Units"] = "Seconds"
+        quat = np.zeros((len(self.tatt), 4))
+        for i, t in enumerate(self.tatt):
+            od = self.orbcorr.orbit_data(t)
+            quat[i, :] = geocal.quaternion_to_array(od.sc_to_ci)
+        t = g.create_dataset("quaternion", data=quat)
+        t.attrs["Description"] = "Attitude quaternion, goes from spacecraft to ECI. The coefficient convention used has the real part in the first column."
+        t.attrs["Units"] = "dimensionless"
+        g = fout.create_group("Ephemeris")
+        t = g.create_dataset("time_j2000", 
+                             data=np.array([t.j2000 for t in self.teph]))
+        pos = np.zeros((len(self.teph), 3))
+        vel = np.zeros((len(self.teph), 3))
+        for i, t in enumerate(self.teph):
+            od = self.orbcorr.orbit_data(t)
+            pos[i, :] = od.position_ci.position
+            vel[i, :] = od.velocity_ci
+        t = g.create_dataset("eci_position", data=pos)
+        t.attrs["Description"] = "ECI position"
+        t.attrs["Units"] = "m"
+        t = g.create_dataset("eci_velocity", data=vel)
+        t.attrs["Description"] = "ECI velocity"
+        t.attrs["Units"] = "m/s"
         m.write()
 
 __all__ = ["L1bAttGenerate"]        
--- python/lib/l1b_att_generate_test.py
+++ python/lib/l1b_att_generate_test.py
@@ -1,10 +1,11 @@
 from .l1b_att_generate import *
 from test_support import *
 
-def test_l1b_att_generate(isolated_dir, igc_old):
-    l1batt = L1bAttGenerate(igc_old, "l1b_att.h5",
+def test_l1b_att_generate(isolated_dir, igc):
+    tatt = [igc.time_table.time(ImageCoordinate(i,0))[0] for i in range(0,44*128, 128)]
+    teph = tatt
+    l1batt = L1bAttGenerate(igc.orbit, "l1b_att.h5", tatt, teph, [],
                             local_granule_id = "ECOSTRESS_L1B_ATT_80001_001_20151024_020211_0100_01.h5")
-
     l1batt.run()
 
 
