e6004c9d0b76788f158e3297ff8f7d8b2155d8e4
Eugene Y Chu <eyc@eco-scf.jpl.nasa.gov>
2018-08-19 15:32:19 +0000

    Add ISS time corrections to L1A_RAW_PIX files.  Address issue 93.

--- python/lib/l1a_raw_pix_generate.py
+++ python/lib/l1a_raw_pix_generate.py
@@ -683,6 +683,13 @@ class L1aRawPixGenerate(object):
       l1a_metag = l1a_fp['/L1A_RAW_PIXMetadata']
       l1a_qamissing = l1a_metag.create_dataset('QAPercentMissingData', data=pcomp, dtype='f4' )
 
+      e0 = np.argmax( rst<terr )
+      e1 = np.argmax( rse<terr )
+      if e1 - e0 > 0:
+        l1a_te=l1a_metag.create_dataset('ISS_time', data=terr[e0:e1], dtype='f8')
+        l1a_td=l1a_metag.create_dataset('ISS_time_dpuio', data=tdpuio[e0:e1], dtype='i8')
+        l1a_tc=l1a_metag.create_dataset('ISS_time_error_correction', data=tcorr[e0:e1], dtype='f8')
+      
       l1a_upg = l1a_fp.create_group("/UncalibratedPixels")
       l1a_ptg = l1a_fp.create_group("/Time")
       l1a_peg = l1a_fp.create_group("/FPIEencoder")
@@ -805,12 +812,14 @@ class L1aRawPixGenerate(object):
     evel.attrs['Units']='m/s'
     attf_met.set('ImageLines', 0)
     attf_met.set('ImagePixels', 0)
+    attf_met.set('SISVersion', '1')
     attf_met.write()
     attf.close()
 
     # Write out a dummy log file
     eng_met.set('ImageLines', 0)
     eng_met.set('ImagePixels', 0)
+    eng_met.set('SISVersion', '1')
     eng_met.write()
     eng.close()
     print("This is a dummy log file", file = self.log)
