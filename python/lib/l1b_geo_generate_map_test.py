a905ad67fba397328577f74a3cdf2df3a48333aa
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2018-09-07 15:31:14 -0700

    Relax tolerance on radiance comparison

--- python/lib/l1b_geo_generate_map_test.py
+++ python/lib/l1b_geo_generate_map_test.py
@@ -13,7 +13,7 @@ import pickle
 def test_l1b_geo_generate_map(isolated_dir, igc, lwm, rad_fname):
     with open('/home/smyth/Local/ecostress-level1/python/l1b_geo_generate.pickle', 'rb') as f:
         l1bgeo = pickle.load(f)
-
+    l1bgeo.output_name="/home/smyth/Local/ecostress-level1/python/l1b_geo.h5"
     l1bgeo_map = L1bGeoGenerateMap(l1bgeo, rad_fname, "l1b_geo_map.h5",
          local_granule_id =
          "ECOSTRESS_L1B_MAP_80001_001_20151024_020211_0100_01.h5")
