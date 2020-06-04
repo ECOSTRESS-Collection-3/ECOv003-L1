3176e9baa646d01a8a21a5e250d853a6df29d63f
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2020-06-04 22:24:45 +0000

    Clean up the creation of 'make dist', we'll need that to create a docker image.

--- bindings/python/cython/cython_init.cc
+++ bindings/python/cython/cython_init.cc
@@ -5,11 +5,15 @@
 extern "C" {
   INIT_TYPE INIT_FUNC(cython_sample)(void);
   INIT_TYPE INIT_FUNC(cython_sample2)(void);
+  INIT_TYPE INIT_FUNC(cython_sample)(void);
+  INIT_TYPE INIT_FUNC(cython_sample2)(void);
 }
 
 static void module_init(PyObject* module)
 {
     PyObject *cython_list = PyList_New(0);
+  INIT_MODULE(cython_list, "ecostress.cython_sample", INIT_FUNC(cython_sample));
+  INIT_MODULE(cython_list, "ecostress.cython_sample2", INIT_FUNC(cython_sample2));
   INIT_MODULE(cython_list, "ecostress.cython_sample", INIT_FUNC(cython_sample));
   INIT_MODULE(cython_list, "ecostress.cython_sample2", INIT_FUNC(cython_sample2));
     PyObject *parent_module = PyImport_AddModule((char *)"ecostress");
