7eec2e163b6a9b8e0f980a8d8e38449438b5ba9a
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2025-09-04 14:57:01 -0700

    Started putting EcostressInterpolateOld in place. Turns out this doesn't work, because tflearn seems to not be supported any more.

---
+++ python/test/unit/ecostress_interpolate_old_test.py
@@ -0,0 +1,22 @@
+from ecostress import EcostressInterpolateOld
+import pytest
+import h5py
+import numpy as np
+
+
+@pytest.mark.long_test
+def test_interpolate(isolated_dir, test_data):
+    # Original test data, we will change this
+    f = h5py.File(
+        test_data + "ECOSTRESS_Simulated_L1B_Kansas_2014168_withDataQuality.h5"
+    )
+    dataset = np.zeros((*f["/SDS/radiance_1"].shape, 5))
+    dqi = np.zeros(dataset.shape)
+    for b in range(5):
+        dataset[:, :, b] = f["/SDS/radiance_%d" % (b + 1)]
+        dqi[:, :, b] = f["/SDS/data_quality_%d" % (b + 1)]
+    print("Done reading data")
+    inter = EcostressInterpolateOld()
+    prediction_matrices, predicted_locations, prediction_errors = (
+        inter.interpolate_missing_bands(dataset, dqi)
+    )
